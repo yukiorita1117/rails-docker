8a635e8e565d5b435eaac61266c79329781edc6f
折田幸夫 <3204704@gmail.com>
2020-03-28 20:35:08 +0900

    rendering 完了

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,6 @@
+# ApplicationContorollerを継承する事でcontorollerとしての役割を持てるっては話
+class BoardsController < ApplicationController
+    # BoardsControllerのindexアクションという(メソッドでもある)
+    def index
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # rootはlocalhost:3000にアクセスが来た際のルーティングを指す。
+  # 今回はlocalhost:3000mのアクセスが来たらBoardsControllerクラスのindexメソッドを実行するように定義。
+  root "boards#index"
 end
