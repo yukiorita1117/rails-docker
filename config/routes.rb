27c02e7350d51b61333b549d5d3dee2a7ea749a5
折田幸夫 <3204704@gmail.com>
2020-04-02 21:36:50 +0900

    add route show

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'boards', to: "boards#index"
   get 'boards/new', to: "boards#new"
   post 'boards', to: "boards#create"
+  get 'boards/:id', to: "boards#show"
 end
