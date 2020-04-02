64ab1725bd2512ff52009e6beaf01588bb6ce2d1
折田幸夫 <3204704@gmail.com>
2020-04-02 22:26:23 +0900

    resource base routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
 
   # rootはlocalhost:3000にアクセスが来た際のルーティングを指す。
   # 今回はlocalhost:3000mのアクセスが来たらBoardsControllerクラスのindexメソッドを実行するように定義。
-  get 'boards', to: "boards#index"
-  get 'boards/new', to: "boards#new"
-  post 'boards', to: "boards#create"
-  get 'boards/:id', to: "boards#show"
+  # get 'boards', to: "boards#index"
+  # get 'boards/new', to: "boards#new"
+  # post 'boards', to: "boards#create"
+  # get 'boards/:id', to: "boards#show"
+
+  # resourcesbase routingを使うと全てのrouteを最初から作れる。
+  resources :boards,only: [:index, :new, :creaste, :show]
+
 end
